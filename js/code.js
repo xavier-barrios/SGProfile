0745f3b95625b54a1d038362a6ec334f5df1ede3
xavier barrios rodriguez <100005770.joan23@fje.edu>
2020-11-30 20:46:27 +0100

    ads

--- js/code.js
+++ js/code.js
@@ -1,16 +1,19 @@
+window.onload = function() {
+    modal = document.getElementById("myModal");
+}
+
 window.onclick = function(event) {
-    var modal = document.getElementById("myModal");
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
 function openModal() {
-    let modal = document.getElementById("myModal");
+    // let modal = document.getElementById("myModal");
     modal.style.display = "block";
 }
 
 function closeModal() {
-    let modal = document.getElementById("myModal");
+    // let modal = document.getElementById("myModal");
     modal.style.display = "none";
 }
