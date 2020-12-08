779adb3b6620c36066878516cfb833d2bfcfa970
xavier barrios rodriguez <100005770.joan23@fje.edu>
2020-12-08 19:15:04 +0100

    codigo modificado

--- view/login.php
+++ view/login.php
@@ -4,20 +4,25 @@
   <link rel="stylesheet" type="text/css" href="../css/login.css">
   <title>Login</title>
   <script src="../js/code.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="login">
+  <br><br>
+<div class="container">
     
   <h2 class="login-header">Iniciar Sesion</h2>
   <!--Creamos el formulario del login, que nos servirá para recoger variables para el userDAO y la funcion del login -->
-    <form class="login-container" action="../controller/loginController.php" method="POST">
-        
-        <p><input type="email" id="email" name="email" placeholder="Email"></p>
+    <form class="form-group" action="../controller/loginController.php" method="POST">
+        <br>
+        <p><input type="email" class="form-control" id="email" name="email" placeholder="Email"></p>
     
-        <p><input type="password" name="password" placeholder="Contraseña"></p>
+        <p><input type="password" class="form-control" name="password" placeholder="Contraseña"></p>
   
-        <input style="margin-bottom: 10px;" type="submit" value="Iniciar Sesión">
-        <button><a href="registro.php">Registrarse</a></button>
+        <input  class="btn btn-primary"  type="submit" value="Iniciar Sesión">
+        <a href="registro.php" class="btn btn-primary">Registrarse</a>
          
       </form>
     </div>
--- view/registro.php
+++ view/registro.php
@@ -5,23 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/login.css">
     <script src="../js/code.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <title>Registro</title>
 </head>
 <body>
-<div class="login" style="margin: 60px auto;">
+<div class="login container" style="margin: 60px auto;">
   <h2 class="login-header">Registrarse</h2>
-    <form class="login-container" action="registro.php" method="POST" onsubmit="return validarFor()">
-        <p><input type="text" class="validar" id="nombre" name="nombre" placeholder="Nombre..."></p>
+    <form  action="registro.php" method="POST" onsubmit="return validarFor()">
+        <p><input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre..."></p>
 
-        <p><input type="text" class="validar" id="apellido" name="apellido" placeholder="Apellido.."></p>
+        <p><input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido.."></p>
 
-        <p><input type="email" class="validar" id="email" name="email" placeholder="Email.."></p>
+        <p><input type="email" class="form-control" id="email" name="email" placeholder="Email.."></p>
 
-        <p><input type="password" class="validar" id="password" name="password" placeholder="Password.."></p>
+        <p><input type="password" class="form-control validar" id="password" name="password" placeholder="Password.."></p>
 
-        <p><input type="password" class="validar" id="password1" name="password1" placeholder="Repite password.."></p>
-        <div style="text-align: center; margin-bottom: 3px;" id="passwd"></div>
-        <input type="submit" value="Crear Cuenta" name="submit">
+        <p><input type="password" class="form-control validar" id="password1" name="password1" placeholder="Repite password.."></p>
+        <div id="passwd"></div>
+        <input type="submit" class="btn btn-primary" value="Crear Cuenta" name="submit">
   </form>
 </div>
 
